046e8b351788ff6cf9aad7740a44175847b442e1
VladVelik <sosinvladik@yandex.ru>
2022-10-28 21:07:09 +0300

    Done

--- ThirdTask/ViewController.swift
+++ ThirdTask/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var currentColorView: UIView!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    @IBOutlet weak var redColorSlider: UISlider!
+    @IBOutlet weak var greenColorSlider: UISlider!
+    @IBOutlet weak var blueColorSlider: UISlider!
+    
+    @IBAction func sliderChange(_ sender: Any) {
+        updateColorView()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateColorView()
     }
-
-
+    
+    private func updateColorView() {
+        currentColorView.layer.cornerRadius = 20
+        
+        currentColorView.backgroundColor = UIColor(
+            red: CGFloat(redColorSlider.value),
+            green: CGFloat(greenColorSlider.value),
+            blue: CGFloat(blueColorSlider.value),
+            alpha: 1.0
+        )
+        
+        redLabel.text = String(format: "%.2f", redColorSlider.value)
+        greenLabel.text = String(format: "%.2f", greenColorSlider.value)
+        blueLabel.text = String(format: "%.2f", blueColorSlider.value)
+    }
 }
-
